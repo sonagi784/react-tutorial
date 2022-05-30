cc2a59928c2410aea60372488f9d6e2a042ad8f5
sonagi784 <sonagi784@gmail.com>
2022-05-30 20:31:42 +0900

    added craco alias for ide

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,14 @@
-const path = require(`path`)
+const CracoAlias = require('craco-alias')
 
 module.exports = {
-  webpack: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: './src',
+        tsConfigPath: './tsconfig.path.json',
+      },
     },
-  },
+  ],
 }
